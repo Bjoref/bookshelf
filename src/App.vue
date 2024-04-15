66a112d4bcd9d2bbbe7bf0338d7b2eeb2e67a428
Bjoref <flamros@yandex.ru>
2024-04-15 19:35:23 +0500

    header + welcome section

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,28 @@
 <template>
-  <section class="section-login" v-if="!login.login">
-    <LogIn/>
+  <the-header 
+    :authorization="authorization.login"
+  />
+  <section class="section-welcome" v-if="!authorization.login">
+    <SectionWelcome/>
   </section>
-  <div v-if="login.login">
+  <div v-if="authorization.login">
     <header>
       <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-  
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-  
-        <nav>
-          <RouterLink to="/">Reading List</RouterLink>
-          <RouterLink to="/finished">Finished Books</RouterLink>
-          <RouterLink to="/about">Finished Books</RouterLink>
-        </nav>
-      </div>
     </header>
-  
+
     <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
-  import { RouterLink, RouterView } from 'vue-router'
-  import HelloWorld from './components/HelloWorld.vue'
-  import LogIn from './components/LogIn.vue'
+import TheHeader from './components/TheHeader.vue'
 
-  import { userAuthorization } from '@/stores/login'
+import { RouterView } from 'vue-router'
+import SectionWelcome from './components/SectionWelcome.vue'
 
-  const login = userAuthorization()
+import { userAuthorization } from '@/stores/login'
+
+const authorization = userAuthorization()
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
